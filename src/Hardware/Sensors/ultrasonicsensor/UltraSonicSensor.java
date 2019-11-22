355d30495d896da1e5f81ce7fd78d095fea9194d
Sem van der Hoeven <semmer99@gmail.com>
2019-11-22 15:00:47 +0100

    comments and changed the initialisation of the timers

--- src/Hardware/Sensors/ultrasonicsensor/UltraSonicSensor.java
+++ src/Hardware/Sensors/ultrasonicsensor/UltraSonicSensor.java
@@ -10,6 +10,10 @@ import java.util.Random;
 //trig pin 7
 //echo pin 8
 
+/**
+ * ultrasonic sensor class.
+ * reads the sensor and {@link this#getCm() saves the distance}
+ */
 public class UltraSonicSensor {
 
     private int echoPin;
@@ -17,36 +21,63 @@ public class UltraSonicSensor {
 
     public Callback distanceCallback;
 
-    Timer timer = new Timer(1);
+    private Timer initTimer;
+    private Timer sensorTimeout;
     private int cm;
 
-    public UltraSonicSensor(int triggerPin, int echoPin) {
+    /**
+     * makes a new sensor
+     *
+     * @param triggerPin the pin to send the trigger signals to
+     * @param echoPin    the pin to read from
+     */
+    public UltraSonicSensor(int triggerPin, int echoPin) throws IllegalArgumentException {
+        if (triggerPin < 0 || echoPin < 0) {
+            throw new IllegalArgumentException("pins can't be negative!");
+        }
+
         this.triggerPin = triggerPin;
         this.echoPin = echoPin;
-        System.out.println("making new sensor");
+//        System.out.println("making new sensor");
 
+        initTimer = new Timer(3);
+        sensorTimeout = new Timer(30);
         distanceCallback = this::listen;
     }
 
+    /**
+     * sends a small pulse to the sensor to initialize it
+     */
     private void initSensor() {
         BoeBot.digitalWrite(this.triggerPin, true);
-        if (timer.timeout()) {
-            BoeBot.digitalWrite(this.echoPin, false);
+        if (initTimer.timeout()) {
+            BoeBot.digitalWrite(this.triggerPin, false);
         }
     }
 
+    /**
+     * listens for a new value from the sensor.
+     */
     public void listen() {
         initSensor();
 
-        int pulse = BoeBot.pulseIn(1, true, 10000);
-//        System.out.println("ultrasonic pulse: " + pulse);
-//        Random random = new Random();
-//        int pulse = random.nextInt();
-        this.cm = pulse / 58;
-//        System.out.println("distance in cm: " + cm);
+        if (sensorTimeout.timeout()) {
 
+            int pulse = BoeBot.pulseIn(echoPin, true, 10000);
+//            System.out.println("ultrasonic pulse: " + pulse);
+//            Random random = new Random();
+//            int pulse = random.nextInt();
+            if (pulse > 0) {
+                this.cm = pulse / 58;
+            }
+        }
     }
 
+    /**
+     * gets the current distance the sensor is reading
+     *
+     * @return the current distance in cm as an <code>int</code>
+     */
     public int getCm() {
         return this.cm;
     }
--- src/interfacelayer/ObjectDetection.java
+++ src/interfacelayer/ObjectDetection.java
@@ -3,14 +3,26 @@ package interfacelayer;
 import Hardware.Sensors.ultrasonicsensor.UltraSonicSensor;
 import OOFramework.FrameworkProgram;
 import OOFramework.StandardObject;
+import TI.Timer;
 
+/**
+ * object detection class to handle the logic for detecting objects
+ */
 public class ObjectDetection extends StandardObject {
     private UltraSonicSensor sensor;
 
+    private Timer timer;
+
+    /**
+     * makes a new objectDetection instance
+     * @param frameworkProgram the program to run it with
+     */
     public ObjectDetection(FrameworkProgram frameworkProgram) {
         super(frameworkProgram);
+
+        timer = new Timer(5);
+        // trigger pin: 7, echo pin: 8
         this.sensor = new UltraSonicSensor(7,8);
-        System.out.println("Making new objectDetection");
     }
 
     @Override
@@ -30,19 +42,25 @@ public class ObjectDetection extends StandardObject {
     }
 
 
+    /**
+     * the main loop where everything is calculated
+     * @param deltaTime the time difference
+     */
     @Override
     protected void mainLoop(double deltaTime) {
         super.mainLoop(deltaTime);
 
         sensor.distanceCallback.run();
 
+        System.out.println(sensor.getCm());
         checkDistance();
     }
 
+    /**
+     * checks the distance that is measured
+     */
     private void checkDistance() {
-        if (sensor.getCm() < 10) {
-            // stop
-        }
+        System.out.println("Stoppen");
     }
 
 

cd9d58dc57825a356f5ffbaca95221a1c7030baf
Sjoerd Jakobs <sjakobs@chello.nl>
2019-11-25 13:55:31 +0100

    fixed reciever( but still untested)

--- src/ButterCat/Modules/Receiver.java
+++ src/ButterCat/Modules/Receiver.java
@@ -1,5 +1,6 @@
 package ButterCat.Modules;
 
+import TI.BoeBot;
 import java.util.ArrayList;
 
 public class Receiver
@@ -7,7 +8,7 @@ public class Receiver
     public void checkForButtonPresses(ArrayList<Button> buttons)
     {
         for (Button button : buttons) {
-           if (pressed) {
+            if (isThisButtonPressed(button.getAddress())) {
                 button.setPressed(true);
                 if (!button.isPressed()) {
                     button.setPressed(true);
@@ -15,11 +16,54 @@ public class Receiver
                 } else if (button.isPressed() && button.isContinuousCallback()) {
                     button.onButtonPress.run();
                 }
-            }
-            else if(button.isPressed())
-            {
+            } else if (button.isPressed()) {
                 button.setPressed(false);
             }
         }
+    }
+
+    private boolean isThisButtonPressed(long address)
+    {
+        int pulseLen = BoeBot.pulseIn(15, false, 6000);
+        long number = 0;
+        if (pulseLen <= 2000) {
+            int lengtes[] = new int[12];
+            for (int i = 0; i < 12; i++) {
+                lengtes[i] = BoeBot.pulseIn(15, false, 20000);
+            }
+            for (int i = 0; i < 12; i++) {
+                System.out.print(lengtes[i]);
+            }
+            number = readButtonReturnInt(lengtes);
+
+            System.out.println("");
+            System.out.println(number);
+        }
+        return (number == address);
+    }
+
+    public long readButtonReturnInt(int lengtes[])
+    {
+        long number = 0;
+        for(int i = 0; i <12; i++)
+        {
+            if(Math.abs(lengtes[i] - 600) < 250)
+            {
+                number += (0 * (Math.pow(10, 11-i)));
+            }
+            else if(Math.abs(lengtes[i] - 1200) < 250)
+            {
+                number +=(1* (Math.pow(10, 11-i)));
+            }
+            else
+            {
+                number +=(2* (Math.pow(10, 11-i)));
+            }
+        }
+        if(number > 219999999999l)
+        {
+            number = -1;
+        }
+        return number;
     }
 }

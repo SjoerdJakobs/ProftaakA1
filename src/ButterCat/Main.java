1fc2f77ae56d275cab636af0545827e194e6af96
kruimeldief <d.iemeel@miracom.nl>
2019-11-27 18:29:28 +0100

    Added 75% of the functions to the ListenToRemote.

--- src/ButterCat/Main.java
+++ src/ButterCat/Main.java
@@ -8,42 +8,7 @@ import TI.BoeBot;
 
 public class Main {
     public static void main(String[] args) {
-        //Program program = new Program();
-        //program.run();
-
-        Engine engine = new Engine(14, 15);
-
-        engine.driveForward();
-        int status = 0;
-
-        while(true) {
-            engine.drive();
-            System.out.println(engine.toString());
-            BoeBot.wait(15);
-
-            if (engine.getMotorRight().getServo().getPulseWidth() == 1700 && status == 0) {
-                engine.turnRight(1);
-                status++;
-                BoeBot.wait(3000);
-            }
-            if (engine.getMotorRight().getServo().getPulseWidth() == 1500 && status == 1) {
-                engine.noTurn();
-                status++;
-                BoeBot.wait(3000);
-            }
-            if (engine.getMotorRight().getServo().getPulseWidth() == 1700 && status == 2) {
-                engine.setEngineTargetSpeed(100);
-                status++;
-                BoeBot.wait(3000);
-            }
-            if (engine.getMotorRight().getServo().getPulseWidth() == 1600 && status == 3) {
-                engine.turnLeft(0.5);
-                status++;
-                BoeBot.wait(3000);
-            }
-
-
-        }
-
+        Program program = new Program();
+        program.run();
     }
 }

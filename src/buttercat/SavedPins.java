eb0fdc76f76a7c3753f4197ab381ebd5e549bcc9
kruimeldief <d.iemeel@miracom.nl>
2019-12-11 16:35:09 +0100

    Saved pins class to make sure there are no dubble pins.

---
+++ src/buttercat/SavedPins.java
@@ -0,0 +1,18 @@
+package buttercat;
+
+public class SavedPins {
+    // Digital pins
+    public static final int ir1Pin = 0;
+    public static final int ir2Pin = 1;
+    public static final int buzzerPin = 2;
+    public static final int ultrasonicEchoPin = 9;
+    public static final int ultrasonicTriggerPin = 10;
+    public static final int buttonPin = 11;
+
+    // Analog pins
+    public static final int lineFollowerLeftPin = 2;
+    public static final int lineFollowerLeftMidPin = 3;
+    public static final int lineFollowerRightMidPin = 1;
+    public static final int lineFollowerRightPin = 0;
+
+}
